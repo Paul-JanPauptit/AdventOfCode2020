54156a767a97270fce5b4d7a1bc422042e0282dd
Paul-Jan Pauptit <paul-jan@eponalabs.com>
2020-12-01 21:39:19 +0100

    Day 1 - Part II - if this goes any deeper than 3 levels we'll need to do a proper rewrite :D

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -10,11 +10,12 @@ namespace Day1
     static void Main()
     {
       var lines = File.ReadAllLines("Input.txt").Select(int.Parse).ToList();
-      var value = FindProductOf2020Parts(lines);
+      var value = FindProductOf2020Parts3(lines);
 
       Console.WriteLine(value);
     }
 
+    /*
     private static int FindProductOf2020Parts(IReadOnlyList<int> lines)
     {
       for (var index1 = 0; index1 < lines.Count; index1++)
@@ -31,6 +32,33 @@ namespace Day1
         }
       }
 
+      return -1;
+    }
+    */
+
+    private static int FindProductOf2020Parts3(IReadOnlyList<int> lines)
+    {
+      for (var index1 = 0; index1 < lines.Count; index1++)
+      {
+        var value1 = lines[index1];
+        for (var index2 = 0; index2 < lines.Count; index2++)
+        {
+          if (index2 == index1)
+            continue;
+
+          var value2 = lines[index2];
+
+          for (var index3 = 0; index3 < lines.Count; index3++)
+          {
+            if (index3 == index2 || index3 == index1)
+              continue;
+
+            var value3 = lines[index3];
+            if (value1 + value2 + value3 == 2020)
+              return value1 * value2 * value3;
+          }
+        }
+      }
       return -1;
     }
   }
