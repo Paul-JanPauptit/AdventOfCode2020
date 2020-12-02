dcaf7b65e76cbf3863c7c68f75c4f2f63b050b08
Paul-Jan Pauptit <paul-jan@eponalabs.com>
2020-12-02 22:15:48 +0100

    Day 2 - Part 2 - XOR Time. You can't stop me! \o/

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -8,15 +8,20 @@ namespace Day2
 {
   class Program
   {
+    private delegate bool IsValidatePasswordDelegate(string password, int value1, int value2, char character);
+
+
     static void Main()
     {
       var lines = File.ReadAllLines("Input.txt").ToList();
-      var value = CountValidPasswords(lines);
+      var value1 = CountValidPasswords(lines, IsValidPassword1);
+      Console.WriteLine(value1);
 
-      Console.WriteLine(value);
+      var value2 = CountValidPasswords(lines, IsValidPassword2);
+      Console.WriteLine(value2);
     }
 
-    private static int CountValidPasswords(List<string> lines)
+    private static int CountValidPasswords(List<string> lines, IsValidatePasswordDelegate isValidatePasswordDelegate)
     {
       var regEx = new Regex(@"(\d*)-(\d*) (.): (.*)");
       var numValid = 0;
@@ -24,14 +29,14 @@ namespace Day2
       foreach (var line in lines)
       {
         var match = regEx.Match(line);
-        if (IsValidPassword(match.Groups[4].Value, Convert.ToInt32(match.Groups[1].Value), Convert.ToInt32(match.Groups[2].Value), match.Groups[3].Value[0]))
+        if (isValidatePasswordDelegate(match.Groups[4].Value, Convert.ToInt32(match.Groups[1].Value), Convert.ToInt32(match.Groups[2].Value), match.Groups[3].Value[0]))
           numValid++;
       }
 
       return numValid;
     }
 
-    private static bool IsValidPassword(string password, int minOccurences, int maxOccurences, char character)
+    private static bool IsValidPassword1(string password, int minOccurences, int maxOccurences, char character)
     {
       var occurences = 0;
       foreach (var c in password)
@@ -40,5 +45,12 @@ namespace Day2
 
       return (minOccurences <= occurences) && (maxOccurences >= occurences);
     }
+
+
+    private static bool IsValidPassword2(string password, int index1, int index2, char character)
+    {
+      return (password[index1 - 1] == character) ^ (password[index2 - 1] == character);
+    }
+
   }
 }
