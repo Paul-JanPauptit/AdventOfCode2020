ec40e3779864805b749c9df20e3ae64119b61b51
Paul-Jan Pauptit <paul-jan@eponalabs.com>
2020-12-06 21:34:03 +0100

    Day 6 - part one

---
+++ Day6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day6
+{
+  class Program
+  {
+    static void Main()
+    {
+      var text = File.ReadAllText("input.txt");
+      var groupsLines = Regex.Split(text, "\r\n\r\n").ToList();
+
+      var numYesses = CountYesses(groupsLines);
+
+      Console.WriteLine($"Part 1: {numYesses}");
+    }
+
+    private static int CountYesses(List<string> groupsLines)
+    {
+      var yesses = 0;
+      foreach (var groupLines in groupsLines)
+      {
+        var groupYesses = new HashSet<char>();
+        var lines = groupLines.Split("\r\n");
+        foreach (var line in lines)
+          groupYesses.UnionWith(line);
+        yesses += groupYesses.Count;
+      }
+
+      return yesses;
+    }
+  }
+}
