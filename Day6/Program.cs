bd7df48df6a58a1e05fd34597442cbdc3b0bd008
Paul-Jan Pauptit <paul-jan@eponalabs.com>
2020-12-06 21:52:02 +0100

    Day 6 - This could've been a lot more elegant if UnionWith was non-mutable (or if there was a hashset-specific Union).

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -13,21 +13,38 @@ namespace Day6
       var text = File.ReadAllText("input.txt");
       var groupsLines = Regex.Split(text, "\r\n\r\n").ToList();
 
-      var numYesses = CountYesses(groupsLines);
+      var numYesses = CountYesses(groupsLines, (s, c) =>
+      {
+        if (s == null)
+          s = new HashSet<char>();
+        s.UnionWith(c);
+        return s;
+      });
 
       Console.WriteLine($"Part 1: {numYesses}");
+
+      numYesses = CountYesses(groupsLines, (s, c) =>
+      {
+        if (s == null)
+          s = new HashSet<char>(c);
+        else
+          s.IntersectWith(c);
+        return s;
+      });
+      Console.WriteLine($"Part 2: {numYesses}");
+
     }
 
-    private static int CountYesses(List<string> groupsLines)
+    private static int CountYesses(List<string> groupsLines, Func<HashSet<char>, IEnumerable<char>, HashSet<char>> combineFunc)
     {
       var yesses = 0;
       foreach (var groupLines in groupsLines)
       {
-        var groupYesses = new HashSet<char>();
+        HashSet<char> groupYesses = null;
         var lines = groupLines.Split("\r\n");
         foreach (var line in lines)
-          groupYesses.UnionWith(line);
-        yesses += groupYesses.Count;
+          groupYesses = combineFunc(groupYesses, line);
+        yesses += groupYesses?.Count ?? 0;
       }
 
       return yesses;
