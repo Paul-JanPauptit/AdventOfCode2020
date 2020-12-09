60839fb1301446f023ffe7d3690b70668280ab93
Paul-Jan Pauptit <paul-jan@eponalabs.com>
2020-12-09 21:21:00 +0100

    Day 9 - Part 2

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -13,6 +13,11 @@ namespace Day9
 
       var firstInvalidNumber = FindFirstInvalidNumber(numbers, 25);
       Console.WriteLine($"Part 1: {firstInvalidNumber}"); // 1038347917
+
+      var parts = FindContiguousRangeForSum(numbers, firstInvalidNumber);
+      var weakness = parts.Min() + parts.Max();
+      Console.WriteLine($"Part 2: {weakness}"); // 137394018
+
     }
 
     private static long FindFirstInvalidNumber(List<long> numbers, int preambleSize)
@@ -28,6 +33,22 @@ namespace Day9
       }
 
       return -1;
+    }
+
+    private static List<long> FindContiguousRangeForSum(List<long> numbers, long total)
+    {
+      for (var startIndex = 0; startIndex < numbers.Count - 1; startIndex++)
+      {
+        // Sum of at least 2 parts.
+        var sum = numbers[startIndex] + numbers[startIndex + 1];
+        var endIndex = startIndex + 2;
+        while (endIndex < numbers.Count && sum < total)
+          sum += numbers[endIndex++];
+        if (sum == total)
+          return numbers.GetRange(startIndex, endIndex - startIndex);
+      }
+
+      throw new Exception($"No contiguous range found for sum {total}");
     }
 
     private static bool IsSumOf(long total, List<long> numbers)
