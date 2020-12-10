f65a095819dc595145a9091a6f66737c6e5c07a5
Paul-Jan Pauptit <paul-jan@eponalabs.com>
2020-12-10 21:14:22 +0100

    Day 10 - part 1

---
+++ Day10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day10
+{
+  class Program
+  {
+    static void Main()
+    {
+      var lines = File.ReadAllLines("Input.txt").Select(int.Parse).ToList();
+      var differences = CountDifferences(lines);
+      var value = differences[1] * differences[3];
+      Console.Write($"Part 1 - {value}");
+    }
+
+    public static Dictionary<int, int> CountDifferences(List<int> lines)
+    {
+      // Look mum, no loops. Also probably no teeth if we did this in production code, but this isn't production code \o/
+      var differences = new int[3];
+      lines.Add(0); // Charging Outlet
+      lines = lines.OrderBy(i => i).ToList();
+      lines.Add(lines.Last() + 3); // Built-in Adapter
+      var deltas = lines.Zip(lines.Skip(1), (current, next) => next - current);
+      return deltas.GroupBy(d => d).OrderBy(d => d.Key).ToDictionary(d => d.Key, d => d.Count());
+    }
+  }
+}
