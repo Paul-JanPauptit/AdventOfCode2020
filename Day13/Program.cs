b9bd09cdb870a2f12aeb94b926d6353881fa817a
Paul-Jan Pauptit <paul-jan@eponalabs.com>
2020-12-19 20:44:13 +0100

    Day 14 - Part 1

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -9,13 +10,14 @@ namespace Day13
     static void Main()
     {
       var lines = File.ReadAllLines("input.txt").ToList();
-      var timestamp = Convert.ToInt32(lines[0]);
-      var busNumbers = lines[1].Split(",").Where(x => x != "x").Select(x => Convert.ToInt32(x));
+      var timestamp = Convert.ToInt64(lines[0]);
+      var busInfos = ReadBusInfos(lines[1]);
       var earliestBusNumber = -1;
       var earliestTimestamp = -1;
-      var magicValue = -1;
-      foreach (var busNumber in busNumbers)
+      var magicValue = -1L;
+      foreach (var busInfo in busInfos)
       {
+        var busNumber = busInfo.Number;
         var nextBusTimestamp = busNumber * (int) Math.Ceiling((float) timestamp / busNumber);
         if (earliestBusNumber == -1 || nextBusTimestamp < earliestTimestamp)
         {
@@ -25,7 +27,57 @@ namespace Day13
         }
       }
 
-      Console.Write($"Part 0 - Arrived at {timestamp}, earliest bus is {earliestBusNumber} at {earliestTimestamp}, value: {magicValue}");
+      Console.WriteLine($"Part 1 - Arrived at {timestamp}, earliest bus is {earliestBusNumber} at {earliestTimestamp}, value: {magicValue}");
+
+      timestamp = 0;
+      var minBusNumber = busInfos.First().Number;
+      var remainingBuses = busInfos.Skip(1).ToList();
+      while (true)
+      {
+        var found = true;
+        foreach (var bus in remainingBuses)
+        {
+          if ((timestamp + bus.Offset) % bus.Number != 0)
+          {
+            found = false;
+            break;
+          }
+        }
+
+        if (found)
+          break;
+
+        timestamp += minBusNumber;
+      }
+
+      Console.WriteLine($"Part 2 - Earliest timestamp: {timestamp}");
+    }
+
+    public static List<BusInfo> ReadBusInfos(string line)
+    {
+      var buses = new List<BusInfo>();
+      var offset = 0;
+      foreach (var value in line.Split(","))
+      {
+        if (int.TryParse(value, out var busNumber))
+        {
+          buses.Add(new BusInfo
+          {
+            Number = busNumber,
+            Offset =  offset
+          });
+        }
+
+        offset++;
+      }
+
+      return buses;
+    }
+
+    public class BusInfo
+    {
+      public int Number { get; set; }
+      public int Offset { get; set; }
     }
   }
 }
---
+++ Day14/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day14
+{
+  class Program
+  {
+    static void Main()
+    {
+      var lines = File.ReadAllLines("input.txt").ToList();
+      var result = ExecuteProgram(lines);
+
+      Console.WriteLine($"Part 0 - {result}");
+    }
+
+    private static long ExecuteProgram(List<string> lines)
+    {
+      var memory = new SortedDictionary<long, long>();
+      var mask = new DualMask();
+      foreach (var line in lines)
+      {
+        if (line.StartsWith("mask = "))
+          mask = DualMask.FromString(line.Substring(7));
+        else
+        {
+          var regEx = new Regex(@"mem\[(\d*)\] = (\d*)");
+          var match = regEx.Match(line);
+          var address = Convert.ToInt64(match.Groups[1].Value);
+          var value = Convert.ToInt64(match.Groups[2].Value);
+          memory[address] = mask.ApplyTo(value);
+        }
+      }
+      return memory.Values.Sum();
+    }
+
+    private class DualMask
+    {
+      private long AndMask { get; set; }
+      private long OrMask { get; set; }
+
+      public static DualMask FromString(string mask)
+      {
+        return new DualMask
+        {
+          AndMask = Convert.ToInt64(mask.Replace("X", "1"), 2),
+          OrMask = Convert.ToInt64(mask.Replace("X", "0"), 2)
+        };
+      }
+
+      public long ApplyTo(long value)
+      {
+        return (value & AndMask) | OrMask;
+      }
+    }
+  }
+}
