cc6a6bf722facb73d6f40726c64b741029348b24
Paul-Jan Pauptit <paul-jan@eponalabs.com>
2021-01-04 21:29:03 +0100

    Day 18 - Part 2

--- Day18/Program.cs
+++ Day18/Program.cs
@@ -9,18 +9,21 @@ namespace Day18
     static void Main()
     {
       var lines = File.ReadAllLines("input.txt").ToList();
-      var result = lines.Sum(Evaluate);
-      
+
+      var result = lines.Sum(Evaluate1);
       Console.WriteLine($"Part 1 - {result}");
+
+      result = lines.Sum(Evaluate2);
+      Console.WriteLine($"Part 2 - {result}");
     }
 
-    private static long Evaluate(string expression)
+    private static long Evaluate1(string expression)
     {
       var index = 0;
-      return Evaluate(expression, ref index);
+      return Evaluate1(expression, ref index);
     }
 
-    private static long Evaluate(string expression, ref int index)
+    private static long Evaluate1(string expression, ref int index)
     {
       var result = 0L;
       var op = Operator.Start;
@@ -35,7 +38,7 @@ namespace Day18
         else if (int.TryParse(character.ToString(), out int value))
           ApplyOperation(value);
         else if (character == '(')
-          ApplyOperation(Evaluate(expression, ref index));
+          ApplyOperation(Evaluate1(expression, ref index));
         else if (character == ')')
           return result;
       }
@@ -57,6 +60,69 @@ namespace Day18
             break;
         }
       }
+    }
+
+    private static long Evaluate2(string expression)
+    {
+      var index = 0;
+      // We don't do proper tokenizing so remove any spaces :)
+      expression = expression.Replace(" ", "");
+
+      return ParseExpression();
+
+      long ParseExpression()
+      {
+        var value = ParseHigherPresedence();
+        if (index < expression.Length)
+        {
+          var token = expression[index];
+          if (token == '*')
+          {
+            index++;
+            value *= ParseExpression();
+          }
+          else if (token == ')') // Hacky unbalanced parser, oh well.
+            index++;
+
+        }
+        return value;
+      }
+
+      long ParseHigherPresedence()
+      {
+        var value = ParseTerm();
+        if (index < expression.Length)
+        {
+          var token = expression[index];
+          if (token == '+')
+          {
+            index++;
+            value += ParseHigherPresedence();
+          }
+        }
+        return value;
+      }
+
+      long ParseTerm()
+      {
+        if (index < expression.Length)
+        {
+          var token = expression[index];
+          if (int.TryParse(token.ToString(), out int number))
+          {
+            index++;
+            return number;
+          }
+
+          if (token == '(')
+          {
+            index++;
+            return ParseExpression();
+          }
+        }
+        return 0;
+      }
+
     }
 
     private enum Operator { Start, Addition, Multiplication }
