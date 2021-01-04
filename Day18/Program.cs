150e5715549a4b2e6acd32176c7280a07e8f0519
Paul-Jan Pauptit <paul-jan@eponalabs.com>
2021-01-04 19:51:00 +0100

    Day 18 - Part 1

---
+++ Day18/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day18
+{
+  class Program
+  {
+    static void Main()
+    {
+      var lines = File.ReadAllLines("input.txt").ToList();
+      var result = lines.Sum(Evaluate);
+      
+      Console.WriteLine($"Part 1 - {result}");
+    }
+
+    private static long Evaluate(string expression)
+    {
+      var index = 0;
+      return Evaluate(expression, ref index);
+    }
+
+    private static long Evaluate(string expression, ref int index)
+    {
+      var result = 0L;
+      var op = Operator.Start;
+      while (index < expression.Length)
+      {
+        var character = expression[index++];
+
+        if (character == '*')
+          op = Operator.Multiplication;
+        else if (character == '+')
+          op = Operator.Addition;
+        else if (int.TryParse(character.ToString(), out int value))
+          ApplyOperation(value);
+        else if (character == '(')
+          ApplyOperation(Evaluate(expression, ref index));
+        else if (character == ')')
+          return result;
+      }
+
+      return result;
+
+      void ApplyOperation(long value)
+      {
+        switch (op)
+        {
+          case Operator.Addition:
+            result += value;
+            break;
+          case Operator.Multiplication:
+            result *= value;
+            break;
+          default:
+            result = value;
+            break;
+        }
+      }
+    }
+
+    private enum Operator { Start, Addition, Multiplication }
+  }
+}
